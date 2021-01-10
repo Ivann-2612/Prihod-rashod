c3531289cd1f1100b09fd7edf05b43f376b7978e
Ivann-2612 <ivan.nikolicamp@gmail.com>
2021-01-10 16:27:17 +0100

    domaci

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dostupanNovac = document.querySelector('#dostupan-novac')
+const dostupanNovac = document.querySelector('#dostupan-novac').value
 const cifraPrihod = document.querySelector('.cifra-prihod')
 const cifraRashod = document.querySelector('.cifra-rashod')
 const procenatRashod = document.querySelector('#procenat-rashod')
@@ -6,41 +6,87 @@ const inputOpis = document.querySelector('#input-opis')
 const inputIznos = document.querySelector('#input-iznos')
 const select = document.querySelector('#select')
 const btn = document.querySelector('#ok-button')
-const display = document.querySelector('#section-three')
 const listaPrihoda = document.querySelector('#listaPrihoda')
 const listaRashoda = document.querySelector('#listaRashoda')
 
 let arrPrihod = []
-//console.log(cifraPrihod);
+let items1 = [0];
+let items2 = [0];
+
+
+function sumArray(total, value) {
+    return total + value;
+}
+
 btn.addEventListener('click', () =>{
+
 
    if(select.value == 'prihod'){
    cifraPrihod.textContent = `+ ${inputIznos.value}`
-   var par = document.createElement('div')
-            par.className = 'list1cont'
-            var par1 = document.createElement('div')
-            par1.className = 'div1'
-            var par13 = document.createElement('div')
-            par13.className = 'div13'
-            var par12 = document.createElement('div')
-            par12.className = 'div12'
-            var obrisi = document.createElement('button')
+
+   const mainDiv = document.createElement('div')
+            mainDiv.className = 'mainDiv1'
+            const div1 = document.createElement('div')
+            div1.className = 'div1'
+            const div2 = document.createElement('div')
+            div2.className = 'div2'
+            const div3 = document.createElement('div')
+            div3.className = 'div3'
+            const obrisi = document.createElement('button')
             obrisi.className = 'obrisi1'
             obrisi.innerHTML = 'x'
-            par.append(par1,par13,par12,obrisi)
-            listaPrihoda.appendChild(par)
-            par1.innerHTML = `${inputOpis.value}`
-            par13.innerHTML = `+`
-            par12.innerHTML = `${(inputIznos.value)}\n`
-            arrPrihod.push(listaPrihoda); 
+            mainDiv.append(div1,div2,div3,obrisi)
+            listaPrihoda.appendChild(mainDiv)
+
+            div1.innerHTML = `${inputOpis.value}`
+            div2.innerHTML = `+`
+            div3.innerHTML = `${inputIznos.value}`
+            items1.push(inputIznos.value * 1);
+            
+        //    for (let i = 0; i < listaPrihoda.length; i++) {
+        //        const sum = 0;
+
+        //      return sum += listaPrihoda[i]
+            
+        //    }
+        //    console.log(sum);   
+            // let sum1 = items1.reduce(sumArray);
+            // let sum2 = items2.reduce(sumArray);
+
+            // document.querySelector('.cifra-prihod').innerHTML = `${sum1} `;
+            // document.querySelector('.cifra-rashod').innerHTML = `${sum2} `;
+            // let sum3 = sum1 - sum2
+            // document.getElementById('#dostupan-novac').innerHTML = `${sum3}`
 }
    else if(select.value == 'rashod'){
    cifraRashod.textContent = `- ${inputIznos.value}`
    
+   const mainDiv2 = document.createElement('div')
+            mainDiv2.className = 'mainDiv2'
+            //    const sum1 = items1.reduce(sumArray);
+            const div21 = document.createElement('div')
+            div21.className = 'div21'
+            const div22 = document.createElement('div')
+            div22.className = 'div22'
+            const div23 = document.createElement('div')
+            div23.className = 'div23'
+            const obrisi1 = document.createElement('button')
+            obrisi1.className = 'obrisi2'
+            obrisi1.innerHTML = 'x'
+            mainDiv2.append(div21,div22,div23,obrisi1)
+            listaRashoda.appendChild(mainDiv2)
+
+            div21.innerHTML = `${inputOpis.value}`
+            div22.innerHTML = `-`
+            div23.innerHTML = `${inputIznos.value} `
+
+            items2.push(inputIznos.value * 1);
+
+            // const sum2 = items2.reduce(sumArray);
+            // const sum3 = sum1 - sum2
+            // document.getElementById('stanjeUkupno').innerHTML = `${sum3}`
+            // document.getElementById('rashodiUkupno').innerHTML = `-${sum2} `
+            //dostupanNovac.innerHTML = cifraPrihod.value - cifraRashod.value
 }
-    
-   
-   //dispay.textContent = createElement()  
+
 })
-console.log(listaPrihoda);
-console.log(arrPrihod);
