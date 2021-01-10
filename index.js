d95ec41cf692862b36af95a274ece67af9ef5898
Ivann-2612 <ivan.nikolicamp@gmail.com>
2021-01-10 13:44:43 +0100

    domaci

--- index.js
+++ index.js
@@ -7,23 +7,40 @@ const inputIznos = document.querySelector('#input-iznos')
 const select = document.querySelector('#select')
 const btn = document.querySelector('#ok-button')
 const display = document.querySelector('#section-three')
+const listaPrihoda = document.querySelector('#listaPrihoda')
+const listaRashoda = document.querySelector('#listaRashoda')
 
-let arr = []
+let arrPrihod = []
 //console.log(cifraPrihod);
 btn.addEventListener('click', () =>{
 
    if(select.value == 'prihod'){
-    
-   cifraPrihod.textContent = `+ ${inputIznos.value}`}
+   cifraPrihod.textContent = `+ ${inputIznos.value}`
+   var par = document.createElement('div')
+            par.className = 'list1cont'
+            var par1 = document.createElement('div')
+            par1.className = 'div1'
+            var par13 = document.createElement('div')
+            par13.className = 'div13'
+            var par12 = document.createElement('div')
+            par12.className = 'div12'
+            var obrisi = document.createElement('button')
+            obrisi.className = 'obrisi1'
+            obrisi.innerHTML = 'x'
+            par.append(par1,par13,par12,obrisi)
+            listaPrihoda.appendChild(par)
+            par1.innerHTML = `${inputOpis.value}`
+            par13.innerHTML = `+`
+            par12.innerHTML = `${(inputIznos.value)}\n`
+            arrPrihod.push(listaPrihoda); 
+}
    else if(select.value == 'rashod'){
-   cifraRashod.textContent = `- ${inputIznos.value}`}
+   cifraRashod.textContent = `- ${inputIznos.value}`
+   
+}
     
-   function createElement(html) {
-       html = `
-       <div id="listaPrihoda">
-         <p class="naslov">Prihodi</p>
-       </div>
-   `
-   }
-   display.textContent = createElement()  
+   
+   //dispay.textContent = createElement()  
 })
+console.log(listaPrihoda);
+console.log(arrPrihod);
