7fbb0297303705a26c252bcfe76269c1b28d615d
Ivann-2612 <ivan.nikolicamp@gmail.com>
2021-01-12 00:27:31 +0100

    domaci novo

--- index.js
+++ index.js
@@ -8,103 +8,77 @@ const btn = document.querySelector('#ok-button')
 const listaPrihoda = document.querySelector('#listaPrihoda')
 const listaRashoda = document.querySelector('#listaRashoda')
 
-let arrPrihod = []
-let items1 = [0];
-let items2 = [0];
+//let arrPrihod = []
+let items1 = [];
+let items2 = [];
 
+btn.addEventListener('click', () => {
 
-function sumArray(total, value) {
-    return total + value;
-}
+    if (select.value == 'prihod') {
+        cifraPrihod.textContent = `+ ${inputIznos.value}`
 
-btn.addEventListener('click', () =>{
+        const mainDiv = document.createElement('div')
+        mainDiv.className = 'mainDiv1'
 
+        mainDiv.innerHTML = `
+                <div class="div1">${inputOpis.value}</div>
+                <div class="div2">`+ `</div>
+                <div class="div3">${inputIznos.value}</div>
+            `
+        const obrisi = document.createElement('button')
+        obrisi.className = 'obrisi1'
+        obrisi.innerHTML = 'x'
+        mainDiv.append(obrisi)
+        mainDiv.style.border = '2px solid black'
+        mainDiv.style.padding = '5px'
+        mainDiv.style.marginTop = '4px'
 
-   if(select.value == 'prihod'){
-   cifraPrihod.textContent = `+ ${inputIznos.value}`
+        listaPrihoda.appendChild(mainDiv)
 
-   const mainDiv = document.createElement('div')
-            mainDiv.className = 'mainDiv1'
-            const div1 = document.createElement('div')
-            div1.className = 'div1'
-            const div2 = document.createElement('div')
-            div2.className = 'div2'
-            const div3 = document.createElement('div')
-            div3.className = 'div3'
-            const obrisi = document.createElement('button')
-            obrisi.className = 'obrisi1'
-            obrisi.innerHTML = 'x'
-            mainDiv.append(div1,div2,div3,obrisi)
-            mainDiv.style.border = '2px solid black'
-            mainDiv.style.padding = '5px'
-            mainDiv.style.marginTop = '4px'
-                
-            listaPrihoda.appendChild(mainDiv)
+        items1.push(inputIznos.value * 1);
 
-            div1.innerHTML = `${inputOpis.value}`
-            div2.innerHTML = `+`
-            div3.innerHTML = `${inputIznos.value}`
-            items1.push(inputIznos.value * 1);
-            
-        //    for (let i = 0; i < listaPrihoda.length; i++) {
-        //        const sum = 0;
-
-        //      return sum += listaPrihoda[i]
-            
-        //    }
-        //    console.log(sum);   
-        const sum1 = items1.reduce(sumArray);
-        const sum2 = items2.reduce(sumArray);
-        const sum3 = sum1 - sum2
-        document.getElementById('dostupanNovac').innerHTML = `${sum3}`
-
-        console.log(sum1);
+        document.querySelector('.cifra-prihod').innerHTML = items1.reduce((a, b) => a + b, 0)
+        
     }
 
-   else if(select.value == 'rashod'){
-   cifraRashod.textContent = `- ${inputIznos.value}`
-   
-   const mainDiv2 = document.createElement('div')
-            mainDiv2.className = 'mainDiv2'
-            //    const sum1 = items1.reduce(sumArray);
-            const div21 = document.createElement('div')
-            div21.className = 'div21'
-            const div22 = document.createElement('div')
-            div22.className = 'div22'
-            const div23 = document.createElement('div')
-            div23.className = 'div23'
-            const obrisi1 = document.createElement('button')
-            obrisi1.className = 'obrisi2'
-            obrisi1.innerHTML = 'x'
-            mainDiv2.append(div21,div22,div23,obrisi1)
-            mainDiv2.style.border = '2px solid black'
-            mainDiv2.style.padding = '5px'
-            mainDiv2.style.marginTop = '4px'
-            listaRashoda.appendChild(mainDiv2)
+    else if (select.value == 'rashod') {
+        cifraRashod.textContent = `- ${inputIznos.value}`
 
-            div21.innerHTML = `${inputOpis.value}`
-            div22.innerHTML = `-`
-            div23.innerHTML = `${inputIznos.value} `
+        const mainDiv2 = document.createElement('div')
+        mainDiv2.className = 'mainDiv2'
 
-            items2.push(inputIznos.value * 1);
-            const sum1 = items1.reduce(sumArray);
-            const sum2 = items2.reduce(sumArray);
-            const sum3 = sum1 - sum2
-            document.getElementById('dostupanNovac').innerHTML = `${sum3}`
-}
-            
-           
-            // dostupanNovac.textContent = cifraPrihod - cifraRashod
+        mainDiv2.innerHTML = `
+                <div class="div21">${inputOpis.value}</div>
+                <div class="div22">`+ `</div>
+                <div class="div23">${inputIznos.value}</div>
+            `
+        const obrisi1 = document.createElement('button')
+        obrisi1.className = 'obrisi2'
+        obrisi1.innerHTML = 'x'
+        mainDiv2.append(obrisi1)
+        mainDiv2.style.border = '2px solid black'
+        mainDiv2.style.padding = '5px'
+        mainDiv2.style.marginTop = '4px'
+        listaRashoda.appendChild(mainDiv2)
+
+        items2.push(inputIznos.value * 1);
+        document.querySelector('.cifra-rashod').innerHTML = items2.reduce((a, b) => a + b, 0)
+
+
+    }
+    let sum = document.querySelector('#dostupan-novac')
+        sum.innerHTML = items1 - items2
 })
 
 
 
 
+//change background color -------------------------------------------------------
 const btnChangeColor = document.querySelector('#change-color')
-const colors = [' red',' #373737',' darkred',' lime',' cyan',' blue',' yellow',' purple',' orangered',' lightgreen',' pink',' azure','navajowhite','gold','lightgreen','lightgoldenrodyellow','indianred','darkred','beige','aquamarine','cadetblue','darkmagenta','black','#141414']
+const colors = [' red', ' #373737', ' darkred', ' lime', ' cyan', ' blue', ' yellow', ' purple', ' orangered', ' lightgreen', ' pink', ' azure', 'navajowhite', 'gold', 'lightgreen', 'lightgoldenrodyellow', 'indianred', 'darkred', 'beige', 'aquamarine', 'cadetblue', 'darkmagenta', 'black', '#141414']
 
 function getRandomColor() {
-    let random  = Math.floor(Math.random() * colors.length)
+    let random = Math.floor(Math.random() * colors.length)
     return random
 }
 //console.log(getRandomColor());
@@ -112,4 +86,7 @@ btnChangeColor.addEventListener('click', () => {
     let randomNumber = getRandomColor()
     document.body.style.height = '100vh'
     document.body.style.backgroundColor = colors[randomNumber]
+    document.querySelector('#change-color').style.backgroundColor = colors[randomNumber]
+    // document.querySelector('h1').style.color = colors[randomNumber]
+
 })
