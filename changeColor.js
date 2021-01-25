6b67262010ad9d46df315733a1496f9ae6dd2d34
Ivan Nikolic <60244706+Ivann-2612@users.noreply.github.com>
2021-01-25 15:59:49 +0100

    Update changeColor.js

--- changeColor.js
+++ changeColor.js
@@ -1,6 +1,6 @@
 //change background color -------------------------------------------------------
 const btnChangeColor = document.querySelector('#change-color')
-const colors = [' red', ' #373737', ' darkred', ' lime', ' cyan', ' blue', ' yellow', ' purple', ' orangered', ' lightgreen', ' pink', ' azure', 'navajowhite', 'gold', 'lightgreen', 'lightgoldenrodyellow', 'indianred', 'darkred', 'beige', 'aquamarine', 'cadetblue', 'darkmagenta', 'black', '#141414']
+const colors = [' red', ' #373737', ' darkred', ' lime', ' cyan','red', ' blue', ' yellow', ' purple', ' orangered', ' lightgreen', ' pink', ' azure', 'navajowhite', 'gold', 'lightgreen', 'lightgoldenrodyellow', 'indianred', 'darkred', 'beige', 'aquamarine', 'cadetblue', 'darkmagenta', 'black', '#141414']
 
 export function getRandomColor() {
     let random = Math.floor(Math.random() * colors.length)
